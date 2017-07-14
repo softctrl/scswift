7271b62147d4226a346089d65642785363b7cee6
timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2017-07-14 09:47:40 -0300

    Add a new method to chow all annotations in a map view.

--- scswift/Classes/MKMapView.swift
+++ scswift/Classes/MKMapView.swift
@@ -22,4 +22,28 @@ extension MKMapView {
 
     }
     
+    
+    /// Based on:
+    /// https://gist.github.com/andrewgleave/915374
+    /// Author: https://gist.github.com/ekussberg
+    public func fitMapViewToAnnotaionList() -> Void {
+
+        let mapEdgePadding = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        var zoomRect:MKMapRect = MKMapRectNull
+        
+        for index in 0..<self.annotations.count {
+            let annotation = self.annotations[index]
+            let aPoint:MKMapPoint = MKMapPointForCoordinate(annotation.coordinate)
+            let rect:MKMapRect = MKMapRectMake(aPoint.x, aPoint.y, 0.1, 0.1)
+            
+            if MKMapRectIsNull(zoomRect) {
+                zoomRect = rect
+            } else {
+                zoomRect = MKMapRectUnion(zoomRect, rect)
+            }
+        }
+        self.setVisibleMapRect(zoomRect, edgePadding: mapEdgePadding, animated: true)
+
+    }
+    
 }
